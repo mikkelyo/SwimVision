e7b2144fc3a0b0d9f1a4a06eb3d4454d045238ab
Christian <s194255@student.dtu.dk>
2020-11-03 11:31:01 +0100

    datasynk

--- objectDetection/trainfasterrcnn.py
+++ objectDetection/trainfasterrcnn.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+
 #vi definerer enheden
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -109,7 +110,7 @@ def train(model,device,root,tran,batch_size=2,
          dataset_train, batch_size=batch_size, shuffle=True, num_workers=0,
          collate_fn=mincollate)
     
-    dataset_validation = SwimSet(os.path.join(root,"validation"),tran)
+    dataset_validation = SwimSet(os.path.join(root,"val"),tran)
     dataloader_validation = torch.utils.data.DataLoader(
         dataset_validation, batch_size=batch_size, shuffle=True, num_workers=0,
         collate_fn=mincollate)
@@ -174,7 +175,7 @@ if __name__ == "__main__":
     
     tran = transforms.Compose([transforms.ToTensor()])
     
-    root = "D:/swimcamD/ObjectDetection/data"
+    root = "../../../SwimData/arucoOctober/objectDetection"
     
     train(model,device,root,tran)
     
