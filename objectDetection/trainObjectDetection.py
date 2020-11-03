93f3e463b927b081bfb26390eb05877c2a2fa3ee
Mikkel Ibsen <mikkel_i@hotmail.com>
2020-11-03 11:56:08 +0100

    testing

--- objectDetection/trainObjectDetection.py
+++ objectDetection/trainObjectDetection.py
@@ -8,7 +8,7 @@ from xml.dom import minidom
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-
+#test
 #vi definerer enheden
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
