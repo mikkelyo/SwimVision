779bdbbb9682583a14a914bd93846d0f32885f07
ChristianEA <christian@swimcam.co>
2020-11-07 18:23:20 +0100

    ændringer i evalObje

--- objectDetection/evalObjectDetection.py
+++ objectDetection/evalObjectDetection.py
@@ -8,40 +8,43 @@ from xml.dom import minidom
 import matplotlib.pyplot as plt
 import numpy as np
 import time 
-from trainfasterrcnn import SwimSet
-from trainfasterrcnn import mincollate
-from trainfasterrcnn import imshow
+from trainObjectDetection import SwimSet
+from trainObjectDetection import mincollate
+from trainObjectDetection import imshow
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-model = torch.load("../../../SwimData/arucoOctober/objectDetection/models/11_12.311.pt",
+model = torch.load("../../../SwimData/SwimCodes/objectDetection/models/14_5.76.pt",
                    map_location=device)
 
 model.eval()
 
 tran = transforms.Compose([transforms.ToTensor()])
 
-dataset_validation = SwimSet("../../../SwimData/arucoOctober/objectDetection/val",tran)
+dataset_validation = SwimSet("../../../SwimData/SwimCodes/objectDetection/val",tran)
 dataloader_validation = torch.utils.data.DataLoader(
  dataset_validation, batch_size=2, shuffle=True, num_workers=0,
  collate_fn=mincollate)
 
 start = time.time()
-i = 1
+batch_count = 1
 for images,targets in dataloader_validation:
-    if images != None:
-        output = model(images)
-        for i in range(len(images)):
-            imshow(images[i])
-            try:
-                punkt = output[i]["boxes"][0]
-                plt.scatter([punkt[0].item(),punkt[2].item()],
-                        [punkt[1].item(),punkt[3].item()],color="r",s=4)
-                plt.show()
-            except IndexError:
-                plt.show()
-            nu = time.time()
-            print(output[i],"\n")
-            #print((2*i)/(nu-start))
-            i += 1
-        
+    with torch.no_grad():
+        if images != None:
+            output = model(images)
+            for i in range(len(images)):
+                imshow(images[i])
+                try:
+                    punkt = output[i]["boxes"][0]
+                    plt.scatter([punkt[0].item(),punkt[2].item()],
+                            [punkt[1].item(),punkt[3].item()],color="r",s=4)
+                    plt.show()
+                except IndexError:
+                    plt.show()
+                print(output[i],"\n")
+            
+            """The underneath can be commented in if one wishes to test inference speed """
+            # nu = time.time()
+            # print((2*batch_count)/(nu-start))
+            # batch_count += 1
+            
