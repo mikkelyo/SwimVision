34b4de7c49190ea5173335ae93920312c8fa7691
Mikkel Ibsen <mikkel_i@hotmail.com>
2020-11-11 15:08:36 +0100

    updated gencodes

--- misc/genSwimCodes_asjpgs.py
+++ misc/genSwimCodes_asjpgs.py
@@ -14,8 +14,8 @@ from cv2 import aruco
 
 class GauBlur(object):
     def __init__(self,p):
-        self.kernelsize = int(np.random.normal(27*2,5))
-        self.sd = abs(np.random.normal(7*1.2,2))
+        self.kernelsize = int(np.random.normal(27*5,5*5))
+        self.sd = abs(np.random.normal(7*1.5,2*1.5))
         self.p = p
         
     
@@ -36,37 +36,6 @@ class convert_to_rgb(object):
         rgb_img = img.convert('RGB')
         return rgb_img
         
-
-
-# class HoriBlur:
-#     def __init__(self,p):
-#         self.kernelsize = int(abs(np.random.normal(19,7)))
-#         self.p = p
-#     def __call__(self,img):
-#         if random.random() < self.p:
-#             kernel = np.zeros((self.kernelsize,self.kernelsize))
-#             kernel[int((self.kernelsize-1)/2),:] = 1/5
-#             slør = cv2.filter2D(np.array(img), -1, kernel)
-#             slør = Image.fromarray(slør)
-#             return slør
-#         else:
-#             return img
-        
-# class VertiBlur:
-#     def __init__(self,p):
-#         self.kernelsize = int(abs(np.random.normal(19,7)))
-#         self.p = p
-#     def __call__(self,img):
-#         if random.random() < self.p:
-#             kernel = np.zeros((self.kernelsize,self.kernelsize))
-#             kernel[:,int((self.kernelsize-1)/2)] = 1/5
-#             slør = cv2.filter2D(np.array(img), -1, kernel)
-#             slør = Image.fromarray(slør)
-#             return slør
-#         else:
-#             return img
-
-
         
 class Blur:
     def __init__(self,p,img):
@@ -116,30 +85,7 @@ class BackGround(object):
     def __init__(self,p,path):
         self.path = path
         self.p = p
-    
-    # def __call__(self,img):
-    #     if random.random() < self.p:
-    #         background = random.choice(os.listdir(self.path))
-    #         background = Image.open(self.path+"/"+background)
-    #         background = background.resize((256,256))
-    #         img = img.convert("RGBA")
-            
-    #         datas = img.getdata()
-    #         newData = []
-    #         for item in datas:
-    #             if item[0] < 30 and item[1] < 30 and item[2] < 30:
-    #                 newData.append((255, 255, 255, 0))
-    #             else:
-    #                 newData.append(item)
-    #         img.putdata(newData)
-            
-    #         background = background.convert("RGBA")
-    #         background.paste(img,(0,0),img)
-    #         background = background.convert("RGB")
-    #         return background
-    #     else:
-    #         return img
-    
+
     def __call__(self,img):
         if random.random() < self.p:
             background = random.choice(os.listdir(self.path))
@@ -147,35 +93,37 @@ class BackGround(object):
             background = background.resize((256,256))
             background = background.convert("RGBA")
             
-            img = img.resize((100,100))
+            img = img.resize((400,400))
             img = img.convert("RGBA")
             
-            points = np.arange(100)
-            x = random.choice(points)
-            y = random.choice(points)
+            points = np.linspace(-75,0,1)
+            x = int(random.choice(points))
+            y = int(random.choice(points))
             background.paste(img,(x,y),img)
             return background
         else:
             return img
 
 
-trans =  transforms.Compose([
-        transforms.Resize((256,256)),
-        GauBlur(0.99),
-        transforms.ColorJitter(brightness=0.3,contrast=0.3,saturation=10,hue=0.5),
-        transforms.RandomRotation(180),
-        transforms.RandomPerspective(p=0.8),
-        transforms.RandomGrayscale(),
-        transforms.RandomHorizontalFlip()])
+# trans =  transforms.Compose([
+#         transforms.Resize((256,256)),
+#         GauBlur(0.99),
+#         transforms.ColorJitter(brightness=0.3,contrast=0.3,saturation=10,hue=0.5),
+#         transforms.RandomRotation(180),
+#         transforms.RandomPerspective(p=0.8),
+#         transforms.RandomGrayscale(),
+#         transforms.RandomHorizontalFlip()])
 
 trans2 = transforms.Compose([transforms.Resize((256,256)),
-                             GauBlur(1),
+                              GauBlur(0.9),
                              transforms.RandomRotation(180),
                              # transforms.RandomGrayscale(),
                              transforms.RandomHorizontalFlip(),
                              transforms.RandomPerspective(p=0.8),
                              BackGround(1,"../../../SwimData/SwimCodes/classification/train/False"),
-                             GauBlur(0.5)
+                              GauBlur(0.6),
+                               # GauBlur(1),
+                               # GauBlur(1)
                              ])
 
 #BackGround(0.9,"../../background")
@@ -198,23 +146,21 @@ data_transforms = {
     ]),
 }
 
-billed = PIL.Image.open("../../../SwimData/SwimCodes/SwimCodes_pngs/A/SwimCode1_transparent.png")
-# # aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_1000)
-# # billed = cv2.aruco.drawMarker(aruco_dict, 500, 256, np.zeros((10,10)), 2)
-# # print(billed.shape)
-# # billed = Image.fromarray(billed)
-# # billed = billed.convert("RGB")
-# plt.imshow(billed)
-# plt.show()
-# nytbild = trans2(billed)
-# plt.imshow(nytbild)
-# plt.show()
+billed = PIL.Image.open("../../../SwimData/SwimCodes/SwimCodes_pngs/B/SwimCode2_transparent.png")
 
-# # background = Image.open("../../background.jpg")
-# # background = background.resize((256,256))
-# # plt.imshow(background)
-# # plt.show()
-# # plt.figure(figsize=(5,5))
-# # plt.imshow(nytbild)
-# # plt.imshow(background,alpha=0.5)
-# # plt.show()
+# plt.imshow$$
+nytbild = trans2(billed)
+plt.imshow(nytbild)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
