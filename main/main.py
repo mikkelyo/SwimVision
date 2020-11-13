a35935ef30638bb6366158f6dabcf089ff6586fe
Christian <s194255@student.dtu.dk>
2020-11-13 17:18:33 +0100

    bokse rundt om koden

--- main/main.py
+++ main/main.py
@@ -9,6 +9,7 @@ from PIL import Image
 import random
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 import torch.nn as nn
+import cv2 
 
 
 #define the device
@@ -31,7 +32,7 @@ objectDetector.to(device)
 #Define the classifier
 classifier = models.vgg19(pretrained=False,progress=False)
 classifier.classifier[6] = nn.Linear(in_features=4096,out_features=len(classNames),bias=True)
-classifier.load_state_dict(torch.load("../../../SwimData/SwimCodes/classification/models/6_0.8320413436692506.pth.pth",
+classifier.load_state_dict(torch.load("../../../SwimData/SwimCodes/classification/models/6_0.8320413436692506.pth",
                                       map_location=device))
 classifier = classifier.to(device)
 
@@ -56,48 +57,52 @@ objectDetectorTrans = transforms.Compose([transforms.ToTensor()])
 
 
 t0 = time.time()
-for i in range(len(filelist)):
-    print("current file: ",filelist[i])
-    #Alle filer follow the pattern resolution_className_index. We extract these using split
-    splits = filelist[i].split("_")
-    
-    #the object detector needs the entire lowRes picture. We load that in as tensor
-    #of shape (1,3,W,H)
-    imagePIL = Image.open(os.path.join(valPath,filelist[i]))
-    image = objectDetectorTrans(imagePIL)
-    image_z = torch.zeros(1,3,image.shape[1],image.shape[2])
-    image_z[0] = image
-    image_z.to(device)
-    
-    #We make inference. Ignore everything other than "boxes"
-    detections = objectDetector(image_z)[0]["boxes"]
-    print("Box points: ",detections)
-    t1 = time.time()
-    print(t1-t0," sec")
-    t0=t1
-    
-    #looping through all suggested boxes
-    for detection in detections:
-        #we need the box points in the highRes picture. For that we need 
-        #the basiskiftematrix
-        box_points = detection.detach().numpy()
-        zoom = imagePIL.crop(box_points[0],box_points[1],box_points[2],box_points[3])
-        #zoom = highRes[int(box_points[1]):int(box_points[3]),int(box_points[0]):int(box_points[2])]
-        try:
-            plt.imshow(np.array(imagePIL))
-            plt.show()
-            plt.imshow(zoom)
-            plt.show()
-            
-            #zoom = Image.fromarray(zoom)
-            zoom = classtrans(zoom)
-            nul = torch.zeros((1,3,256,256))
-            nul[0] = zoom
-            nul = nul.to(device)
-            outputs = classifier(nul)
-            _, preds = torch.max(outputs, 1)
-            print("I predict: ",classNames[preds])
-            
-        except ValueError:
-            print("tomt zoom")
-    print("\n")
+with torch.no_grad():
+    for i in range(len(filelist)):
+        print("current file: ",filelist[i])
+        #Alle filer follow the pattern resolution_className_index. We extract these using split
+        splits = filelist[i].split("_")
+        
+        #the object detector needs the entire lowRes picture. We load that in as tensor
+        #of shape (1,3,W,H)
+        imagePIL = Image.open(os.path.join(valPath,filelist[i]))
+        image = objectDetectorTrans(imagePIL)
+        image_z = torch.zeros(1,3,image.shape[1],image.shape[2])
+        image_z[0] = image
+        image_z.to(device)
+        
+        #We make inference. Ignore everything other than "boxes"
+        detections = objectDetector(image_z)[0]["boxes"]
+        print("Box points: ",detections)
+        t1 = time.time()
+        print(t1-t0," sec")
+        t0=t1
+        
+        #looping through all suggested boxes
+        for detection in detections:
+            #we need the box points in the highRes picture. For that we need 
+            #the basiskiftematrix
+            box_points = detection.detach().numpy()
+            zoom = imagePIL.crop((box_points[0],box_points[1],box_points[2],box_points[3]))
+            try:
+                detectedImage = cv2.rectangle(np.array(imagePIL),(int(box_points[0]),int(box_points[1])),
+                                              (int(box_points[2]),int(box_points[3])),color=(255,255,0),
+                                              thickness=5)
+                plt.imshow(detectedImage)
+                plt.show()
+                
+                plt.imshow(zoom)
+                plt.show()
+                
+                #zoom = Image.fromarray(zoom)
+                zoom = classtrans(zoom)
+                nul = torch.zeros((1,3,256,256))
+                nul[0] = zoom
+                nul = nul.to(device)
+                outputs = classifier(nul)
+                _, preds = torch.max(outputs, 1)
+                print("I predict: ",classNames[preds])
+                
+            except ValueError:
+                print("tomt zoom")
+        print("\n")
